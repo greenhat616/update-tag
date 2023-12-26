4f3e78db5e49cae58fa1c3058ebaa6bce1981de5
greenhat616 <i@a632079.me>
2023-12-27 01:49:49 +0800

    feat: add ref resolver

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function run() {
   try {
     const { GITHUB_SHA, GITHUB_TOKEN } = process.env;
     const tagName = core.getInput('tag_name');
-    if (!GITHUB_SHA) {
+    const ref: string | undefined = core.getInput('ref') || GITHUB_SHA;
+    if (!ref) {
       core.setFailed('Missing GITHUB_SHA');
       return;
     }
@@ -21,9 +22,27 @@ async function run() {
     }
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
-    let ref;
+
+    let commitSha = ref;
+    if (commitSha.length !== 40) {
+      try {
+        const refs = await octokit.rest.git.listMatchingRefs({
+          ...github.context.repo,
+          ref,
+        });
+        if (refs.data.length === 0) {
+          core.setFailed(`No matching refs found for ${ref}`);
+          return;
+        }
+        commitSha = refs.data[0].object.sha; // Use the first match
+      } catch (e) {
+        core.setFailed(e.message || e);
+      }
+    }
+    console.log(`😬 Update tag with ref hash ${commitSha}.`);
+    let tagRef;
     try {
-      ref = await octokit.rest.git.getRef({
+      tagRef = await octokit.rest.git.getRef({
         ...github.context.repo,
         ref: `tags/${tagName}`,
       });
@@ -34,19 +53,21 @@ async function run() {
         throw e;
       }
     }
-    if (!ref) {
+    if (!tagRef) {
+      console.log(`😕 Tag ${tagName} does not exist. Creating...`);
       await octokit.rest.git.createRef({
         ...github.context.repo,
         ref: `refs/tags/${tagName}`,
-        sha: GITHUB_SHA,
+        sha: commitSha,
       });
     } else {
       await octokit.rest.git.updateRef({
         ...github.context.repo,
         ref: `tags/${tagName}`,
-        sha: GITHUB_SHA,
+        sha: commitSha,
       });
     }
+    console.log(`😊 Tag ${tagName} updated.`);
   } catch (error) {
     core.setFailed(error.message);
   }
